41252b0ebbc1366005d57c29f4a46914386a8550
BinWiz <78808159+n1ck007@users.noreply.github.com>
2023-02-27 08:34:25 -0600

    added iterative bubble sort

--- sorting.cpp
+++ sorting.cpp
@@ -12,6 +12,20 @@ int isSorted(int arr[], int n){
     return arr[n-1] >= arr[n-2] && isSorted(arr, n - 1);
 }
 
-
-
+// Bubble Sort - Iterative
+void bubbleSort(int arr[], int n) {
+    // this loop controls the number of passes
+    for (int k = 0; k < n - 1; k++) {
+        // after x pass we know that the last x items will be sorted
+        for (int i = 0; i < n - 1 - k; i++) {
+            // if wrong order
+            if (arr[i] > arr[i + 1]) {
+                // swap
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+    }
+}
 
