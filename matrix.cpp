e7ac35ce23e53c46e6e5a220b562e61da10ce577
BinWiz <78808159+n1ck007@users.noreply.github.com>
2023-03-06 11:26:45 -0600

    Create matrix.cpp

---
+++ matrix.cpp
@@ -0,0 +1,21 @@
+// Dynamic Matrix using std::vector
+vector<vector<int>> mat = {
+        { 1,   2, 3,   4, 5 },
+        { 16, 17, 18, 19, 6 },
+        { 15, 24, 25, 20, 7 },
+        { 14, 23, 22, 21, 8 },
+        { 13, 12, 11, 10, 9 }
+    };
+
+
+// Static Matrix
+int mat[3][4] = {
+  { 0, 1,  2,  3 }, 
+  { 4, 5,  6,  7 }, 
+  { 8, 9, 10, 11 }
+};
+
+
+// Dynamic Matrix 
+// https://stackoverflow.com/questions/1403150/how-do-you-dynamically-allocate-a-matrix
+
