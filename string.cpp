2c1e5bfa1eed13aae5d57ce4bd2a0bb30f184db0
BinWiz <78808159+n1ck007@users.noreply.github.com>
2023-03-07 13:25:46 -0600

    Update string.cpp

--- string.cpp
+++ string.cpp
@@ -3,4 +3,9 @@
 string str;
 
 // .erase(starting index, length of substr to remove, )
-str.erase()
+str.erase();
+  
+// make lowercase
+for (size_t i = 0; i < str.size(); i++) {
+		str.at(i) = tolower(str.at(i));
+}
