77d29a24b3a17a499024813a6f5cd9b54f909a5c
BinWiz <78808159+n1ck007@users.noreply.github.com>
2023-03-10 17:28:24 -0600

    Update string.cpp

--- string.cpp
+++ string.cpp
@@ -1,22 +1,37 @@
 #include<string>
 
-string str;
+// processes the user's response to a yes or no question
+bool getYesNo() {
+	string str;
+	getline(cin, str);
 
-// erase part of a string 
-// .erase(starting index, length of substr to remove, )
-str.erase();
-  
-// erase whitespace
-string eraseWhiteSpace(string str) {
-    for (size_t i = str.size() - 1; i >= 0; i--) {
-        if (str[i] == ' ') {
-	    str.erase(i,1);
+	// must test for a zero size first or else the for loops be out_of_range
+	if (str.size() == 0) {
+		cout << "Input cannot be understood. Please try again: ";
+		return getYesNo();
 	}
-    }
-    return str;
-}
 
-// make lowercase
-for (size_t i = 0; i < str.size(); i++) {
+	// erase whitespace
+	for (size_t i = str.size() - 1; i > 0; i--) {
+		if (str.at(i) == ' ') {
+			str.erase(i, 1);
+		}
+	}
+
+	// make lowercase
+	for (size_t i = 0; i < str.size(); i++) {
 		str.at(i) = tolower(str.at(i));
+	}
+
+
+	if ( str == "y" || str == "yes" ) {
+		return true;
+	}
+	else if ( str == "n" || str == "no" ) {
+		return false;
+	}
+	else {
+		cout << "Input cannot be understood. Please try again: ";
+		return getYesNo();
+	}
 }
