392addeca3c5dc8b2e23ecee3ef5c812149a97a7
BinWiz <78808159+n1ck007@users.noreply.github.com>
2023-03-10 15:48:03 -0600

    Update string.cpp

--- string.cpp
+++ string.cpp
@@ -2,9 +2,20 @@
 
 string str;
 
+// erase part of a string 
 // .erase(starting index, length of substr to remove, )
 str.erase();
   
+// erase whitespace
+string eraseWhiteSpace(string str) {
+    for (size_t i = str.size() - 1; i >= 0; i--) {
+        if (str[i] == ' ') {
+	    str.erase(i,1);
+	}
+    }
+    return str;
+}
+
 // make lowercase
 for (size_t i = 0; i < str.size(); i++) {
 		str.at(i) = tolower(str.at(i));
