b03b76485ec7c89bbf4799d8860eebe4b88a25d5
BinWiz <78808159+n1ck007@users.noreply.github.com>
2023-03-14 16:50:22 -0500

    removed redundant zero size check
    also added more valid responses

--- user-input.cpp
+++ user-input.cpp
@@ -2,31 +2,25 @@ bool getYesNo() {
 	string str;
 	getline(cin, str);
 
-	// must test for a zero size first or else the for loops be out_of_range
-	if (str.size() == 0) {
-		cout << "Input cannot be understood. Please try again: ";
-		return getYesNo();
-	}
-
-	// erase whitespace or make lowercase
-	for (size_t i = str.size() - 1; i > 0; i--) {
-		if (str.at(i) == ' ') {
-			str.erase(i, 1);
+	if (str.size() != 0) {
+		// erase whitespace or make lowercase
+		for (size_t i = str.size() - 1; i > 0; i--) {
+			if (str.at(i) == ' ') {
+				str.erase(i, 1);
+			}
+			else {
+				str.at(i) = tolower(str.at(i));
+			}
 		}
-		else {
-			str.at(i) = tolower(str.at(i));
+
+		if (str == "y" || str == "yes" || "t" || "true") {
+			return true;
+		}
+		else if (str == "n" || str == "no" || "f" || "false") {
+			return false;
 		}
 	}
 
-
-	if ( str == "y" || str == "yes" ) {
-		return true;
-	}
-	else if ( str == "n" || str == "no" ) {
-		return false;
-	}
-	else {
-		cout << "Input cannot be understood. Please try again: ";
-		return getYesNo();
-	}
+	cout << "Input cannot be understood. Please try again: ";
+	return getYesNo();
 }
