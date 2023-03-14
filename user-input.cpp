af183e23d8e28029f47ea24163d7e24d49b5b424
BinWiz <78808159+n1ck007@users.noreply.github.com>
2023-03-14 16:41:05 -0500

    revisions and simplifications

--- user-input.cpp
+++ user-input.cpp
@@ -1,34 +1,34 @@
-// processes the user's response to a yes or no question
 bool getYesNo() {
 	string str;
 	getline(cin, str);
 
-	// if nothing is entered or the input more than 3 characters, or the first char is a space, prompt again
-	while (str.size() < 1 || str.size() > 3 || str.at(0) == ' ') {
+	// must test for a zero size first or else the for loops be out_of_range
+	if (str.size() == 0) {
 		cout << "Input cannot be understood. Please try again: ";
-		getline(cin, str);
+		return getYesNo();
+	}
 
-		while (!(str.size() < 1 || str.size() > 3 || str.at(0) == ' ')) {
-			
-			// erase whitespace
-			for (size_t i = str.size() - 1; i > 0; i--) {
-				if (str.at(i) == ' ') {
-					str.erase(i, 1);
-				}
-			}
-			// is they answered yes
-			if (str.at(0) == 'Y' || str.at(0) == 'y' || str.at(0) == '1' ) {
-				return true;
-			}
-			// if they answered no
-			else if (str.at(0) == 'N' || str.at(0) == 'n' || str.at(0) == '0') {
-				return false;
-			}
-			// if unclear
-			else {
-				cout << "Input cannot be understood. Please try again: ";
-				getline(cin, str);
-			}
+	// erase whitespace
+	for (size_t i = str.size() - 1; i > 0; i--) {
+		if (str.at(i) == ' ') {
+			str.erase(i, 1);
 		}
+	}
+
+	// make lowercase
+	for (size_t i = 0; i < str.size(); i++) {
+		str.at(i) = tolower(str.at(i));
+	}
+
+
+	if ( str == "y" || str == "yes" ) {
+		return true;
+	}
+	else if ( str == "n" || str == "no" ) {
+		return false;
+	}
+	else {
+		cout << "Input cannot be understood. Please try again: ";
+		return getYesNo();
 	}
 }
