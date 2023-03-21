341c69817e053cbcd57bf46c035f0e039d493a28
BinWiz <78808159+n1ck007@users.noreply.github.com>
2023-03-21 12:44:23 -0500

    added string to int

--- type-conversion.cpp
+++ type-conversion.cpp
@@ -9,6 +9,10 @@ string myStr(1, myChar);
 string myStr = "743";
 int myInt = stoi(myStr, 0, 10); 
 
+// convert int to string
+int myInt = 101;
+str = to_string(myInt);
+
 // convert size_t to int
 size_t life = 42;
 static_cast<int>(life);
