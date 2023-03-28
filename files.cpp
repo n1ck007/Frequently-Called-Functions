a8751a487cfea0f279b1a2cf9c4abab075e6ed06
BinWiz <78808159+n1ck007@users.noreply.github.com>
2023-03-28 08:47:45 -0500

    minor adjustments

--- files.cpp
+++ files.cpp
@@ -3,23 +3,23 @@
 #include<string>
 
 int main() {
-  fstream file;
-  string fileName;
-  string line;
+	fstream file;
+  	string fileName;
+  	string line;
   
-  cout << "Enter the file name: ";
+  	cout << "Enter the file name: ";
 	getline(cin, fileName);
   
   
-  // read from file
-  file.open(fileName, ios::in);
-  if (file.is_open()) {
+  	// read from file
+  	file.open(fileName, ios::in);
+  	if (file.is_open()) {
+		// print every line from the file
 		while (getline(file, line)) {
-			cout << line << endl;
-    }
-  }
-  
-  
+		cout << line << endl;
+    		}
+  	}
+	file.close();
   return 0;
 }
   
