4d78dee46aa6a128dd4168457c48d2661c3fa179
BinWiz <78808159+n1ck007@users.noreply.github.com>
2023-04-17 10:57:55 -0500

    removed verbose comments

--- type-conversion.cpp
+++ type-conversion.cpp
@@ -1,11 +1,9 @@
-// Convert char to string 
+// convert char to string 
 // using the string constructor String(length, char)
 char myChar = 'a';
 string myStr(1, myChar);
 
-// Convert String to int
-// using stoi(string, starting position, number base) 
-// 0 and 10 are the default values so entering explictly is not necessary
+// convert string to int
 string myStr = "743";
 int myInt = stoi(myStr, 0, 10); 
 
